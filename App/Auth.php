45e3809c7881d37fe596f5e2c6c2d6615cbcac8d
Maciej <maciejklosinski2@gmail.com>
2023-04-22 08:17:27 +0200

    initial commit, added directories

---
+++ App/Auth.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+// Authentication 
+//PHP version 7.4
+
+class Auth
+{
+    
+}
---
+++ Core/Router.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Core;
+
+//Router
+//PHP version 7.4
+
+class Router
+{
+   
+}
