956e6886ef22200e1183fac1a2b0910ebedee927
Maciej <maciejklosinski2@gmail.com>
2023-05-10 22:13:40 +0200

    Update View & Controler for new designed Mainpage with additional links and watchers for Balance

--- App/Controllers/Mainpage.php
+++ App/Controllers/Mainpage.php
@@ -5,6 +5,8 @@ namespace App\Controllers;
 use \Core\View;
 use \App\Models\User;
 use \App\Auth;
+use \App\Models\Balance;
+use \App\Flash;
 
 class Mainpage extends Authenticated
 {
@@ -16,13 +18,19 @@ class Mainpage extends Authenticated
     }
     
 	public function mainpageAction()
-	{				
+	{
+		$currentMonthDate = Balance::getCurrentMonthDate();
+		$lastMonthDate = Balance::getlastMonthDate();
+		$currentMonthBalance = Balance::getBalance($currentMonthDate,$this->user->id);
+		$lastMonthBalance = Balance::getBalance($lastMonthDate,$this->user->id);
 		$username = User::getUsername($this->user->id);
 		
 		View::renderTemplate('Mainpage/mainpage.html',
 			[
 				'user' => $this->user,
-				'username' => $username				
+				'username' => $username,				
+				'currentMonthBalance' => $currentMonthBalance,
+				'lastMonthBalance' => $lastMonthBalance
 			]);
 	}
 }
